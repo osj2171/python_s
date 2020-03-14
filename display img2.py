5b87a4deb2a489c5e5226aa186c5d202ddb9edaf
osj2171 <osj2171@naver.com>
2020-03-14 16:07:09 +0900

    크기 맞춰 출력

--- display img2.py
+++ display img2.py
@@ -5,9 +5,12 @@ import PIL.ImageTk
 
 def disp_img(path):
     newimage = PIL.Image.open(path).resize( (300,300) )
-    imageData = PIL.ImageTk.PhotoImage(newimage)
+    gimage = PIL.Image.open(path).convert("L").resize( (250,250) )
+    mimage = PIL.Image.open(path).convert("L").resize( (32,32) ).resize( (250,250) )
+
+    imageData=PIL.ImageTk.PhotoImage(mimage)
     imageLabel.configure(image=imageData)
-    imageLabel = imageData
+    imageLabel.image = imageData
 
     fpathLabel.configure(text=path)
 
@@ -22,7 +25,6 @@ root = tk.Tk()
 root.geometry("400x350")
 
 btn = tk.Button(text="파일 열기", command=open_file)
-imageLabel.pack()
 fpathLabel = tk.Label()
 imageLabel = tk.Label()
 btn.pack()
